2e6275f21d41b586dc3cee19fc3674f505276140
yota.hatano <yota.hatano@wealthnavi.com>
2025-10-16 08:15:13 +0900

    fix: 検証

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
             // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
             System.out.println("i = " + i);
+
+            //TIP コード補完を使用するには、<shortcut actionId="CodeCompletion"/> を押します。
+            System.out.println("i squared = " + (i * i));
         }
     }
 }
